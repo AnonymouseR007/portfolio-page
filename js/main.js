d13a1a63bbf3852dc9b5c726365f4c61590b6273
AnonymouseR007 <112969719+AnonymouseR007@users.noreply.github.com>
2023-03-04 11:05:40 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+ 
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
+
+let burgerBtn = document.querySelector(".burger-menu-btn");
+let burgerMenu = document.querySelector(".burger-menu");
+
+
+let isBurgerOpen = false;
+
+burgerBtn.onclick = function () {
+    if (!isBurgerOpen) {
+        burgerMenu.style.display = "block";
+        burgerBtn.style.backgroundPosition = "center left 50px, center";
+        isBurgerOpen = true;
+    }
+    else if (isBurgerOpen) {
+        burgerMenu.style.display = "none";
+        burgerBtn.style.backgroundPosition = "center, center left 50px";
+        isBurgerOpen = false;
+    }
+}
+// let burgerBtn = document.querySelector(".burger-menu-btn");
+// let burgerMenu = document.querySelector(".burger-menu");
+
+// burgerBtn.onclick = function () {
+//   burgerMenu.classList.toggle("open");
+//   burgerBtn.classList.toggle("open");
+// };
